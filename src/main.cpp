9dfe6e85b19bf2fc620c8bcc9a93e57bc6af6f0a
Acul4321 <lucarogers666@gmail.com>
2026-04-07 21:51:13 +0100

    layout and pin def

--- src/main.cpp
+++ src/main.cpp
@@ -4,33 +4,39 @@
 #include "components/Button.hpp"
 #include "components/Encoder.hpp"
 
-Button* button = Button::getInstance(4, 'z');
-Encoder* encoder = Encoder::getInstance(0, 1, '3', '4');
+Encoder* VOL_L = Encoder::getInstance(0, 1, '3', '4');
+Encoder* VOL_R = Encoder::getInstance(2, 3, '0', '-');
+
+Button* BT_A = Button::getInstance(4, 'e');
+Button* BT_B = Button::getInstance(5, 'r');
+Button* BT_C = Button::getInstance(6, 'o');
+Button* BT_D = Button::getInstance(7, 'p');
+
+Button* FX_L = Button::getInstance(8, 'v');
+Button* FX_R = Button::getInstance(9, ',');
 
 void setup() {
   Serial.begin(9600);
   Keyboard.begin();
-  button->init();
-  encoder->init();
+
+  BT_A->init();
+  BT_B->init();
+  BT_C->init();
+  BT_D->init();
+  FX_L->init();
+  FX_R->init();
+  VOL_L->init();
+  VOL_R->init();
 }
 
 void loop() {
-  button->updateKeyBinding();
-  encoder->updateKeyBinding();
+  BT_A->updateKeyBinding();
+  BT_B->updateKeyBinding();
+  BT_C->updateKeyBinding();
+  BT_D->updateKeyBinding();
 
-  if (button->isPressed()) {
-    Serial.println("Button is PRESSED");
-  } else {
-    Serial.println("Button is RELEASED");
-  }
-
-  Encoder::Direction dir = encoder->getDirection();
-  if (dir == Encoder::Direction::Left) {
-    Serial.println("Encoder turned LEFT");
-    encoder->resetDirection();
-  } else if (dir == Encoder::Direction::Right) {
-    Serial.println("Encoder turned RIGHT");
-    encoder->resetDirection();
-  }
-
+  VOL_L->updateKeyBinding();
+  VOL_R->updateKeyBinding();
+  FX_L->updateKeyBinding();
+  FX_R->updateKeyBinding();
 }
